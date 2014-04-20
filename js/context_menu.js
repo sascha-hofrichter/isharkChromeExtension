a2eb7261a86aa760d629f234239533f86e176266
PhR3Ak <hofrichter.sascha@gmail.com>
2014-04-20 17:36:59 +0200

    Add ajax-request to ishark server

--- js/context_menu.js
+++ js/context_menu.js
@@ -1,4 +1,42 @@
 function createContextMenu() {
     chrome.contextMenus.removeAll();
-    chrome.contextMenus.create({'title': 'Upload to iShark', 'contexts': ['image', 'link']});
+    chrome.contextMenus.create({'title': 'Upload to iShark', 'contexts': ['image', 'link'], 'onclick': uploadToIshark});
+}
+
+
+function uploadToIshark(info, tab) {
+    if(!getOriginalImgUrl()) {
+        console.error('can not upload the image');
+    } else {
+        // 'upload' image to iShark
+        $.ajax({
+            url: 'http://ishark.dev/web/api/uploadURL',
+            type: 'POST',
+            timeout: 1000,
+            data: {url: getOriginalImgUrl()}
+
+        }).done(function (response) {
+            setiSharkImgUrl(response.url);
+            console.log(response.url);
+        }).fail(function (response, textStatus, errorThrown) {
+            if(response.status > 0) {
+                switch (response.status) {
+                    case 422:
+                        // url was not set
+                        console.error('no URL to image');
+                        break;
+
+                    default:
+                    case 500:
+                        // server internal error
+                        console.error('Internal server error');
+                        break;
+                }
+            } else {
+                // have a timeout or server are not available
+                console.error('iShark-Server is not available');
+            }
+        });
+    }
+
 }
